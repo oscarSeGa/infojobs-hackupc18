291701d0061728d95c7b67a1d25a431a736b68a1
OCTAVI <octavi.prats@est.fib.upc.edu>
2018-10-21 06:19:01 +0200

    cv

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -4,14 +4,6 @@ import { CvService } from './cv.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
-export interface LanguageElement {
-  languages_name: string;
-  languages_speaking: string;
-  languages_reading: string;
-  languages_writing: string;
-}
-
-var ELEMENT_DATA: LanguageElement[] = [];
 
 @Component({
   selector: 'app-cv',
@@ -21,23 +13,36 @@ var ELEMENT_DATA: LanguageElement[] = [];
 })
 export class CvComponent implements OnInit {
 
+  public firstForm: FormGroup;
+  public cv: any = [];
   public sub1: Subscription;
-  public cv: any;
-  public errorMessage: String;
-  public displayedColumns: string[] = ['languages_name', 'languages_speaking', 'languages_reading', 'languages_writing'];
-  public dataSource = ELEMENT_DATA;
+  public errorMessage: string = ""
 
-  constructor(private cvService: CvService) { }
+  constructor(private fb: FormBuilder, private cvService: CvService) { }
 
   ngOnInit() {
+    console.log("DEBUG");
     this.sub1 = this.cvService.getCV().subscribe(
       res => {
         this.cv = res;
-        console.info("CV", this.cv.languages);
-        ELEMENT_DATA = this.cv.languages;
+        console.log(this.cv);
       },
       error => this.errorMessage = <any>error
     );
+    this.firstForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: [''],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      birthday: ['', Validators.required],
+      gender: [''],
+      country: ['', Validators.required],
+      cp: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(5)])],
+      place: ['', Validators.required],
+      phone: ['', Validators.compose([Validators.minLength(9), Validators.maxLength(9)])],
+      github: [''],
+      bitbucket: [''],
+      url: ['']
+    });
   }
 
 }
